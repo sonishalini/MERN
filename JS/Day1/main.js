a0b697aed73bd511819ca1cb62afac7f06fc0e86
sonishalini <shalinisoni25@gmail.com>
2022-02-16 19:25:21 +0530

    javascript

---
+++ JS/Day1/main.js
@@ -0,0 +1,26 @@
+/*
+fundamental (basic)
+functions
+array logic
+string logic
+API 
+ Event loop
+ DOM
+ Events 
+ Promise 
+ xhr (ajax)
+ fetch (http client promises)
+ aysnc await
+ localStorage
+ */
+
+ /* Es6 features
+ oop
+ advance funtion
+ ds
+ spreed
+ getter & setter
+ rest method
+  */
+
+/* react  */ 
